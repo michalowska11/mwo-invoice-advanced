e67f9b772b60f232ad1d11af0bba472f82a713ca
student11 <student11@S7N-PROGLANG-9.pcoip.ki.agh.edu.pl>
2017-02-12 11:35:10 +0100

    Sukces dla produktu!

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,6 +10,10 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null || name.equals("")||price==null || price.compareTo(BigDecimal.ZERO)<0) {
+			throw new IllegalArgumentException();
+		} 
+		
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
